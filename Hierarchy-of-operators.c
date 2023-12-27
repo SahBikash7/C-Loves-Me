302e78d607a839dddedd0535821b57e86fdd1a8c
Bikash <sahb2676@gmail.com>
2023-12-27 22:55:16 +0545

    Hierarchy Of Operators

---
+++ Hierarchy-of-operators.c
@@ -0,0 +1,9 @@
+// Hierarchy Of Operators:Same weight ko operators xa bhane left to right process garxa computer leh.
+// 2*3/4=6/4
+
+#include<stdio.h>
+int main(){
+    int i=2*3/4+4/4+8-2+5/8;
+    printf("The value of i is %d\n",i);
+    return 0;
+}
