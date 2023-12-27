4296c3cf66a93ab139a65fc5585e4fe60c1eae57
Bikash <sahb2676@gmail.com>
2023-12-27 23:16:18 +0545

    What actually is IF statement

---
+++ the-real-thing-of-if-statements.c
@@ -0,0 +1,17 @@
+// The Real Thing
+
+#include<stdio.h>
+int main(){
+    int a;
+    if(3+2%5)
+        printf("This Works.\n");
+    if(a=10)
+        printf("Even this works.\n");
+    if(-5)
+        printf("Surprisingly even this works.\n");
+    if(0)
+        printf("LOL.\n");
+    else
+        printf("Hello.\n");
+    return 0;
+}
