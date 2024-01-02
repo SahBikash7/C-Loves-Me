a078dc13e8607eba17647d43175effc17c215521
Bikash <sahb2676@gmail.com>
2024-01-02 13:32:07 +0545

    Predict The Output6

---
+++ predict-output6.c
@@ -0,0 +1,9 @@
+// Predict The Output:
+#include<stdio.h>
+int main(){
+    while('a'<'b')
+        printf("\nmalayalam is a palindrome");
+    return 0;
+}
+// MY Observation:Actual Output
+// The ASCII Value of a and b will be compared a=97 and b=98 and 97<98 so it will go in infinite loop and keep printing the printf statement..
