3b49fa4ecab4f789f3e6bb6a888bb0a7f0cf6565
Bikash <sahb2676@gmail.com>
2024-01-02 13:25:48 +0545

    Predict The Output5

---
+++ predict-output5.c
@@ -0,0 +1,16 @@
+// Predict The Output:
+#include<stdio.h>
+int main(){
+    int x=4,y=3,z;
+    z=x-- -y;
+    printf("\n%d %d %d",x,y,z);
+    return 0;
+}
+
+// My Observation:Actual Output
+// z=x-- -y
+// z=4-3
+// z=1
+// so:
+
+// 3 3 1
