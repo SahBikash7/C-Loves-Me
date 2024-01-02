5b973b82db980b1ad80939c0e699397cf9a88eeb
Bikash <sahb2676@gmail.com>
2024-01-02 13:12:40 +0545

    Predict The Output3

---
+++ predict-output3.c
@@ -0,0 +1,14 @@
+// Predict The Output:
+#include<stdio.h>
+int main(){
+    int x=1;
+    while(x==1){
+        x=x-1;
+        printf("\n%d",x);
+    }
+    return 0;
+}
+
+// MY Observation:Actual Output
+
+// 0
