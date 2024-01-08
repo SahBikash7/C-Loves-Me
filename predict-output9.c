3fdee1bf9f06a5d9e693c02dd7dc8a35a2f20a70
Bikash <sahb2676@gmail.com>
2024-01-08 06:34:51 +0545

    Predict the output9

---
+++ predict-output9.c
@@ -0,0 +1,20 @@
+// Predict the Output:
+#include<stdio.h>
+int main(){
+    int x=4,y=0,z;
+   while(x>=0){
+        x--;
+        y++;
+        if(x==y)
+            continue;
+        else
+            printf("\n%d %d",x,y);
+   }
+    return 0;
+}
+
+// My Observation: Actual Output:
+// 3 1
+// 1 3
+// 0 4
+// -1 5
